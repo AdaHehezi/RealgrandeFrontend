ae5335e0332077ebbcbfa4e5fb14c3b186eff343
AdaHehezi <hehezi2011@gmail.com>
2023-11-07 22:41:33 -0800

    frontend

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <footer className="bg-light text-dark p-3">
+        <div className="text-center">
+            <a class="btn btn-outline-dack btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
+            <a class="btn btn-outline-dack btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
+            <a class="btn btn-outline-dack btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
+        </div>            
+        
+        <div className="copyright py-4 text-center text-dack">
+            <div className="container"><small>Copyright &copy; RealGrande </small></div>
+        </div>
+        </footer>
+    );
+}
+ 
+export default Footer;
